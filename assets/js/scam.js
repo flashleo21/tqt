bb2554cdf16656ed074df5d04e146dd8cab56cff
flashleo21 <leo.guerin.2011@gmail.com>
2024-06-20 23:44:07 +0200

    commit4

--- assets/js/scam.js
+++ assets/js/scam.js
@@ -1,25 +1,26 @@
 let changeColorBtn = document.getElementById('changeColorBtn');
-let title = document.getElementById('title');
 
-changeColorBtn.addEventListener('mousemove touchmove', (e) => {
-    let randomPositionTop = Math.floor(Math.random() * 100) + '%';
-    let randomPositionLeft = Math.floor(Math.random() * 100) + '%';
-
-    changeColorBtn.style.position = 'absolute';
-    changeColorBtn.style.top = `${randomPositionTop}`;
-    changeColorBtn.style.left = `${randomPositionLeft}`;
+changeColorBtn.addEventListener('mousemove', (e) => {
+  let randomPositionTop = Math.floor(Math.random() * 300);
+  let randomPositionRight = Math.floor(Math.random() * 500);
+  let randomPositionBottom = Math.floor(Math.random() * 300);
+  let randomPositionLeft = Math.floor(Math.random() * 500);
+  changeColorBtn.style.top = `${randomPositionTop}px`;
+  changeColorBtn.style.right = `${randomPositionRight}px`;
+  changeColorBtn.style.bottom = `${randomPositionBottom}px`;
+  changeColorBtn.style.left = `${randomPositionLeft}px`;
 });
 
 setTimeout(() => {
-    changeColorBtn.remove();
-    title.remove();
-    let message = document.createElement('div');
-    message.textContent = 'Tu as bien vu que ça ne fonctionne pas?';
-    message.style.position = 'fixed';
-    message.style.top = '50%';
-    message.style.left = '50%';
-    message.style.transform = 'translate(-50%, -50%)';
-    message.style.fontSize = '5vw'; 
-    message.style.color = 'red';
-    document.body.appendChild(message);
-}, 12000);
+  changeColorBtn.remove();
+
+  let message = document.createElement('div');
+  message.textContent = 'Tu as bien vu que ça ne fonctionne pas ?';
+  message.style.position = 'absolute';
+  message.style.top = '50%';
+  message.style.left = '50%';
+  message.style.transform = 'translate(-50%, -50%)';
+  message.style.fontSize = '24px';
+  message.style.color = 'red';
+  document.body.appendChild(message);
+}, 20000);
